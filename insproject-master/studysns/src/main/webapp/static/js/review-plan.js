28d72dc4bbd9fbe35e45c62a2b87d5060d3d8cfe
yangbin <465471659@qq.com>
2018-09-03 10:05:47 +0800

    合并修改

--- insproject-master/studysns/src/main/webapp/static/js/review-plan.js
+++ insproject-master/studysns/src/main/webapp/static/js/review-plan.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 var ws = new WebSocket("ws:www.nizi521.club:99/studysns/websocket/notes");
-=======
-var ws = new WebSocket("ws://127.0.0.1/studysns/websocket/notes");
->>>>>>> fdd57bd248689cd62f64af3d8ea99d76c27f24b8
-
 ws.onmessage = function(evt) {
 	  console.log( "Received Message: " + evt.data);
 	  var data = JSON.parse(evt.data);
